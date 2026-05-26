82cd16952627ffaa30ee34f4263955eaa7cf7838
Brayan Chavez <brayanjairchavez981@gmail.com>
2026-05-25 23:06:27 -0500

    feat: initial salesflow crm setup

---
+++ src/main/java/com/brayan/salesflow/SalesflowCrmApiApplication.java
@@ -0,0 +1,13 @@
+package com.brayan.salesflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesflowCrmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesflowCrmApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brayan/salesflow/SalesflowCrmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brayan.salesflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesflowCrmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
